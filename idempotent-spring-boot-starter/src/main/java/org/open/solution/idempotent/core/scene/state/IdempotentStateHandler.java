2aa70b2458ac0aeafc57f425a8677559d63bb62c
Marko <13815560463@163.com>
2023-07-12 13:20:26 +0800

    code fix

--- idempotent-spring-boot-starter/src/main/java/org/open/solution/idempotent/core/scene/state/IdempotentStateHandler.java
+++ idempotent-spring-boot-starter/src/main/java/org/open/solution/idempotent/core/scene/state/IdempotentStateHandler.java
@@ -55,7 +55,7 @@ public class IdempotentStateHandler
       String state = stringRedisTemplate.opsForValue().get(lockKey);
 
 
-      if (state == null) {
+      if (!StringUtils.hasLength(state)) {
         // state 为null的情况，以下两种情况对consumingExpirationDate的设置合理性要高，才能避免为null
         // 1.上面设置超时运行到此处正好过期，这种情况不可能，consumingExpirationDate为业务逻辑时间要长，而且这几行代码没有耗时，除非redis极慢
         // 2.被其他线程调用exceptionProcessing，也不会，正常情况如果consumingExpirationDate值合理，异常后都是当前线程来调用，然而exceptionProcessing在当前方法后面执行
@@ -87,8 +87,15 @@ public class IdempotentStateHandler
           // 根据validateIdempotent中对于null的情况，基于合理的consumingExpirationDate值，可以得出当state为null时
           // 说明当前线程执行业务逻辑时触发异常被删除，因此为了使得后续合理的重试请求可以得到继续，则保持未消费的状态。
           String state = stringRedisTemplate.opsForValue().get(wrapper.param.getLockKey());
-          if (StringUtils.hasLength(state) && IdempotentStateEnum.CONSUMING.getCode().equals(state)) {
+
+          if (!StringUtils.hasLength(state)) {
+            wrapper.param.getLogger().warn("[{}] this key consumption period is too short.", wrapper.param.getLockKey());
+          }
+
+          if (IdempotentStateEnum.CONSUMING.getCode().equals(state)) {
             consumed(wrapper.param);
+          } else if (IdempotentStateEnum.CONSUMED.getCode().equals(state)) {
+            wrapper.param.getLogger().error("[{}] this key already consumed.", wrapper.param.getLockKey());
           }
         }
       } catch (Throwable ex) {
